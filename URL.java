fc6b76e4db6c57d619fde81ca2004b22b888c046
mandeep <mandeepnain84@gmail.com>
2022-04-27 19:32:19 +0530

    Formatting done, URL,USERNAME,PASSWORD in different class

---
+++ URL.java
@@ -0,0 +1,7 @@
+package com.java_sql_connection;
+
+public class URL {
+    static String url = "jdbc:mysql://localhost:3306/LibraryManagementSystem";
+    static String user = "mandeepnain";
+    static String password = "Qwertyui@123";
+}
--- java_sql.java
+++ java_sql.java
@@ -1,21 +1,16 @@
 package com.java_sql_connection;
 
 import java.sql.*;
-
 import java.time.LocalDate;
-
 import java.util.Scanner;
+import static com.java_sql_connection.URL.*;
 
 public class java_sql {
-
-    static String url = "jdbc:mysql://localhost:3306/LibraryManagementSystem";
-    static String user = "mandeepnain";
-    static String password = "Qwertyui@123";
-
     static LocalDate cdate = LocalDate.now();
-
-    public static void main(String args[]) {
-
+    public static void main(String[] args) {
+        Start();
+    }
+    static void Start(){
         Scanner sc = new Scanner(System.in);
         int key;
         while (true) {
@@ -26,8 +21,6 @@ public class java_sql {
             System.out.println("4. Submit Book");
             //scan key
             key = sc.nextInt();
-
-
             switch (key) {
                 case 1:
                     IssueBook();
@@ -42,13 +35,9 @@ public class java_sql {
                     SubmitBook();
                     break;
                 default:
-                    throw new IllegalStateException("Unexpected value: " + key);
+                    System.out.println("Try Again!");
             }
         }
-    }
-
-    void Start(){
-
     }
     public static void IssueBook(){
         System.out.println("You want to Issue a Book.");
@@ -58,85 +47,60 @@ public class java_sql {
         System.out.println("Please enter Book id");
         //scan book id
         int book_id = sc.nextInt();
-
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection is Successful to the database" + url);
-
             String query = String.format("Insert into Checkout(S_id,B_id, checkout_date) values( %d,%d,'%s')", student_id, book_id, cdate);
             String query1 = "Update Books set available = available - 1 where book_id ="+book_id+";";
             Statement statement = connection.createStatement();
             statement.execute(query);
             statement.execute(query1);
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
         }
-
-
     }
-
     static void AvailableBooks(){
         System.out.println("List of Books: ");
         String books;
-
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection is Successful to the database" + url);
-
             String query = "Select name from Books";
             Statement statement = connection.createStatement();
             books = String.valueOf(statement.execute(query));
             System.out.println(books);
-
-
-
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
         }
-
     }
-
     static void RegisterLostBook(){
         System.out.println("You want to register lost book.");
         System.out.println("Enter Student id who lost it.");
         Scanner sc = new Scanner(System.in);
         //scan student id
         int student_id = sc.nextInt();
-
         System.out.println("Enter Book id");
         //scan book id
         int book_id = sc.nextInt();
-
-
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection is Successful to the database" + url);
-
             String query = String.format("Insert into Book_Lost(b_id,s_id, checkout_date) values( %d,%d,'%s')", student_id, book_id, cdate);
             Statement statement = connection.createStatement();
             statement.execute(query);
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
         }
-
     }
-
     static void SubmitBook(){
         System.out.println("You want to Submit Book to Library");
         System.out.println("Enter book id");
         //scan book id
         Scanner sc = new Scanner(System.in);
         int book_id = sc.nextInt();
-
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, user, password);
@@ -145,12 +109,9 @@ public class java_sql {
             String query = "Update Books set available = available +1 where book_id = "+book_id+";";
             Statement statement = connection.createStatement();
             statement.execute(query);
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
         }
     }
-
 }
 
